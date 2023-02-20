5db8b47360d0e37468a48df3b1e58b468fe0924b
izzoel <ramadhani.zulfahmi@gmail.com>
2023-02-20 10:50:00 +0800

    update database sql

--- database/tracer.sql
+++ database/tracer.sql
@@ -23,6 +23,130 @@ CREATE TABLE IF NOT EXISTS `admins` (
 /*!40000 ALTER TABLE `admins` DISABLE KEYS */;
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
 
+-- Dumping structure for table tracer.bank_alumnis
+CREATE TABLE IF NOT EXISTS `bank_alumnis` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `bank1` text COLLATE utf8mb4_unicode_ci,
+  `bank2` text COLLATE utf8mb4_unicode_ci,
+  `bank3` text COLLATE utf8mb4_unicode_ci,
+  `bank4` text COLLATE utf8mb4_unicode_ci,
+  `bank5` text COLLATE utf8mb4_unicode_ci,
+  `bank6` text COLLATE utf8mb4_unicode_ci,
+  `bank7` text COLLATE utf8mb4_unicode_ci,
+  `bank8` text COLLATE utf8mb4_unicode_ci,
+  `bank9` text COLLATE utf8mb4_unicode_ci,
+  `bank10` text COLLATE utf8mb4_unicode_ci,
+  `bank11` text COLLATE utf8mb4_unicode_ci,
+  `bank12` text COLLATE utf8mb4_unicode_ci,
+  `bank13` text COLLATE utf8mb4_unicode_ci,
+  `bank14` text COLLATE utf8mb4_unicode_ci,
+  `bank15` text COLLATE utf8mb4_unicode_ci,
+  `bank16` text COLLATE utf8mb4_unicode_ci,
+  `bank17` text COLLATE utf8mb4_unicode_ci,
+  `bank18` text COLLATE utf8mb4_unicode_ci,
+  `bank19` text COLLATE utf8mb4_unicode_ci,
+  `bank20` text COLLATE utf8mb4_unicode_ci,
+  `bank21` text COLLATE utf8mb4_unicode_ci,
+  `bank22` text COLLATE utf8mb4_unicode_ci,
+  `bank23` text COLLATE utf8mb4_unicode_ci,
+  `bank24` text COLLATE utf8mb4_unicode_ci,
+  `bank25` text COLLATE utf8mb4_unicode_ci,
+  `bank26` text COLLATE utf8mb4_unicode_ci,
+  `bank27` text COLLATE utf8mb4_unicode_ci,
+  `bank28` text COLLATE utf8mb4_unicode_ci,
+  `bank29` text COLLATE utf8mb4_unicode_ci,
+  `bank30` text COLLATE utf8mb4_unicode_ci,
+  `bank31` text COLLATE utf8mb4_unicode_ci,
+  `bank32` text COLLATE utf8mb4_unicode_ci,
+  `bank33` text COLLATE utf8mb4_unicode_ci,
+  `bank34` text COLLATE utf8mb4_unicode_ci,
+  `bank35` text COLLATE utf8mb4_unicode_ci,
+  `bank36` text COLLATE utf8mb4_unicode_ci,
+  `bank37` text COLLATE utf8mb4_unicode_ci,
+  `bank38` text COLLATE utf8mb4_unicode_ci,
+  `bank39` text COLLATE utf8mb4_unicode_ci,
+  `bank40` text COLLATE utf8mb4_unicode_ci,
+  `bank41` text COLLATE utf8mb4_unicode_ci,
+  `bank42` text COLLATE utf8mb4_unicode_ci,
+  `bank43` text COLLATE utf8mb4_unicode_ci,
+  `bank44` text COLLATE utf8mb4_unicode_ci,
+  `bank45` text COLLATE utf8mb4_unicode_ci,
+  `bank46` text COLLATE utf8mb4_unicode_ci,
+  `bank47` text COLLATE utf8mb4_unicode_ci,
+  `bank48` text COLLATE utf8mb4_unicode_ci,
+  `bank49` text COLLATE utf8mb4_unicode_ci,
+  `bank50` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table tracer.bank_alumnis: ~0 rows (approximately)
+/*!40000 ALTER TABLE `bank_alumnis` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bank_alumnis` ENABLE KEYS */;
+
+-- Dumping structure for table tracer.bank_lulusans
+CREATE TABLE IF NOT EXISTS `bank_lulusans` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `bank1` text COLLATE utf8mb4_unicode_ci,
+  `bank2` text COLLATE utf8mb4_unicode_ci,
+  `bank3` text COLLATE utf8mb4_unicode_ci,
+  `bank4` text COLLATE utf8mb4_unicode_ci,
+  `bank5` text COLLATE utf8mb4_unicode_ci,
+  `bank6` text COLLATE utf8mb4_unicode_ci,
+  `bank7` text COLLATE utf8mb4_unicode_ci,
+  `bank8` text COLLATE utf8mb4_unicode_ci,
+  `bank9` text COLLATE utf8mb4_unicode_ci,
+  `bank10` text COLLATE utf8mb4_unicode_ci,
+  `bank11` text COLLATE utf8mb4_unicode_ci,
+  `bank12` text COLLATE utf8mb4_unicode_ci,
+  `bank13` text COLLATE utf8mb4_unicode_ci,
+  `bank14` text COLLATE utf8mb4_unicode_ci,
+  `bank15` text COLLATE utf8mb4_unicode_ci,
+  `bank16` text COLLATE utf8mb4_unicode_ci,
+  `bank17` text COLLATE utf8mb4_unicode_ci,
+  `bank18` text COLLATE utf8mb4_unicode_ci,
+  `bank19` text COLLATE utf8mb4_unicode_ci,
+  `bank20` text COLLATE utf8mb4_unicode_ci,
+  `bank21` text COLLATE utf8mb4_unicode_ci,
+  `bank22` text COLLATE utf8mb4_unicode_ci,
+  `bank23` text COLLATE utf8mb4_unicode_ci,
+  `bank24` text COLLATE utf8mb4_unicode_ci,
+  `bank25` text COLLATE utf8mb4_unicode_ci,
+  `bank26` text COLLATE utf8mb4_unicode_ci,
+  `bank27` text COLLATE utf8mb4_unicode_ci,
+  `bank28` text COLLATE utf8mb4_unicode_ci,
+  `bank29` text COLLATE utf8mb4_unicode_ci,
+  `bank30` text COLLATE utf8mb4_unicode_ci,
+  `bank31` text COLLATE utf8mb4_unicode_ci,
+  `bank32` text COLLATE utf8mb4_unicode_ci,
+  `bank33` text COLLATE utf8mb4_unicode_ci,
+  `bank34` text COLLATE utf8mb4_unicode_ci,
+  `bank35` text COLLATE utf8mb4_unicode_ci,
+  `bank36` text COLLATE utf8mb4_unicode_ci,
+  `bank37` text COLLATE utf8mb4_unicode_ci,
+  `bank38` text COLLATE utf8mb4_unicode_ci,
+  `bank39` text COLLATE utf8mb4_unicode_ci,
+  `bank40` text COLLATE utf8mb4_unicode_ci,
+  `bank41` text COLLATE utf8mb4_unicode_ci,
+  `bank42` text COLLATE utf8mb4_unicode_ci,
+  `bank43` text COLLATE utf8mb4_unicode_ci,
+  `bank44` text COLLATE utf8mb4_unicode_ci,
+  `bank45` text COLLATE utf8mb4_unicode_ci,
+  `bank46` text COLLATE utf8mb4_unicode_ci,
+  `bank47` text COLLATE utf8mb4_unicode_ci,
+  `bank48` text COLLATE utf8mb4_unicode_ci,
+  `bank49` text COLLATE utf8mb4_unicode_ci,
+  `bank50` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table tracer.bank_lulusans: ~0 rows (approximately)
+/*!40000 ALTER TABLE `bank_lulusans` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bank_lulusans` ENABLE KEYS */;
+
 -- Dumping structure for table tracer.failed_jobs
 CREATE TABLE IF NOT EXISTS `failed_jobs` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -46,18 +170,21 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table tracer.migrations: ~7 rows (approximately)
+-- Dumping data for table tracer.migrations: ~10 rows (approximately)
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-	(1, '2014_10_12_000000_create_users_table', 1),
-	(2, '2014_10_12_100000_create_password_resets_table', 1),
-	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-	(5, '2023_02_01_125844_create_survey_alumnis_table', 1),
-	(6, '2023_02_08_023802_create_admins_table', 2),
-	(7, '2023_02_15_151316_create_survey_lulusans_table', 2);
+	(11, '2014_10_12_000000_create_users_table', 1),
+	(12, '2014_10_12_100000_create_password_resets_table', 1),
+	(13, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(14, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+	(15, '2023_02_01_125844_create_survey_alumnis_table', 1),
+	(16, '2023_02_08_023802_create_admins_table', 1),
+	(17, '2023_02_15_151316_create_survey_lulusans_table', 1),
+	(18, '2023_02_15_161059_create_user_alumnis_table', 1),
+	(19, '2023_02_17_125220_create_bank_alumnis_table', 1),
+	(20, '2023_02_19_102632_create_bank_lulusans_table', 1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 
 -- Dumping structure for table tracer.password_resets
@@ -98,26 +225,17 @@ CREATE TABLE IF NOT EXISTS `survey_alumnis` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `no` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `survey` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pilihan` text COLLATE utf8mb4_unicode_ci,
-  `ganda` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
-  `other` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pilihan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ganda` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `other` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `wajib` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table tracer.survey_alumnis: ~8 rows (approximately)
+-- Dumping data for table tracer.survey_alumnis: ~0 rows (approximately)
 /*!40000 ALTER TABLE `survey_alumnis` DISABLE KEYS */;
-INSERT INTO `survey_alumnis` (`id`, `no`, `survey`, `pilihan`, `ganda`, `other`, `wajib`, `created_at`, `updated_at`) VALUES
-	(1, '7', 'penilaian', '5;4;3;2;1', '0', '0', '1', '2023-02-14 11:04:05', '2023-02-15 15:05:00'),
-	(5, '2', 'Apakah', 'y;t', '0', '0', '1', '2023-02-14 12:17:21', '2023-02-15 10:43:14'),
-	(6, '3', 'Umur', '21;22;23', '0', '1', '1', '2023-02-14 13:19:32', '2023-02-15 10:43:32'),
-	(7, '5', 's', '', '0', '0', '1', '2023-02-14 13:23:32', '2023-02-15 10:44:05'),
-	(8, '1', 'abcd', 'ef;e', '1', '1', '1', '2023-02-14 14:26:32', '2023-02-15 10:49:28'),
-	(9, '6', 'abcd zxczxc\r\nasdasd\r\nasdasd', 'a', '0', '0', '1', '2023-02-14 16:25:05', '2023-02-15 15:50:13'),
-	(12, '8', 'aefds', '', '0', '0', '1', '2023-02-15 15:46:15', '2023-02-15 15:46:15'),
-	(13, '9', 'asdsa', 'saddas;asdas', '0', '1', '1', '2023-02-15 15:51:58', '2023-02-15 15:51:58');
 /*!40000 ALTER TABLE `survey_alumnis` ENABLE KEYS */;
 
 -- Dumping structure for table tracer.survey_lulusans
@@ -132,16 +250,10 @@ CREATE TABLE IF NOT EXISTS `survey_lulusans` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table tracer.survey_lulusans: ~5 rows (approximately)
+-- Dumping data for table tracer.survey_lulusans: ~0 rows (approximately)
 /*!40000 ALTER TABLE `survey_lulusans` DISABLE KEYS */;
-INSERT INTO `survey_lulusans` (`id`, `no`, `survey`, `pilihan`, `ganda`, `other`, `wajib`, `created_at`, `updated_at`) VALUES
-	(1, '1', 'nama', '', '0', '0', '1', '2023-02-15 15:32:32', '2023-02-15 15:32:32'),
-	(2, '2', 's', '', '0', '0', '1', '2023-02-15 15:32:41', '2023-02-15 15:32:41'),
-	(3, '3', 'as', '', '0', '0', '1', '2023-02-15 15:35:59', '2023-02-15 15:46:33'),
-	(4, '4', 'zx\r\nzx\r\nzzz\r\nz\r\n\r\n\r\n\r\nz', '', '0', '0', '1', '2023-02-15 15:36:10', '2023-02-15 15:52:56'),
-	(6, '5', 'zxqadwqw', 'asdsa', '0', '1', '1', '2023-02-15 15:53:29', '2023-02-15 15:53:29');
 /*!40000 ALTER TABLE `survey_lulusans` ENABLE KEYS */;
 
 -- Dumping structure for table tracer.users
@@ -149,20 +261,29 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table tracer.users: ~1 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `name`, `password`, `email`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 'admin', '$2y$10$P5/r.KGtmPGTdRwvvPJeCuqhxVqyNNW6NFz8ABPM5oksHoTWfM5EO', NULL, NULL, NULL, '2023-02-08 11:41:02', '2023-02-08 11:41:02');
+INSERT INTO `users` (`id`, `name`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'admin', '$2y$10$P5/r.KGtmPGTdRwvvPJeCuqhxVqyNNW6NFz8ABPM5oksHoTWfM5EO', NULL, NULL, NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping structure for table tracer.user_alumnis
+CREATE TABLE IF NOT EXISTS `user_alumnis` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table tracer.user_alumnis: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user_alumnis` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_alumnis` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
